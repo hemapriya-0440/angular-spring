2f77c7305f42c890f5ae2a48fe08812bd83d8f07
unknown <hemapriya0440@gmail.com>
2020-09-25 17:55:13 +0530

    with few update fields

---
+++ src/app/employee.service.ts
@@ -0,0 +1,34 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class EmployeeService {
+
+  private baseUrl = 'http://localhost:8080/springboot-crud-rest/api/v1/employees';
+
+  constructor(private http: HttpClient) { }
+
+  getEmployee(empId: number): Observable<any> {
+    return this.http.get(`${this.baseUrl}/${empId}`);
+  }
+
+  createEmployee(employee: Object): Observable<Object> {
+    return this.http.post(`${this.baseUrl}`, employee);
+  }
+
+  updateEmployee(empId: number, value: any): Observable<Object> {
+    return this.http.put(`${this.baseUrl}/${empId}`, value);
+  }
+  
+
+  deleteEmployee(empId: number): Observable<any> {
+    return this.http.delete(`${this.baseUrl}/${empId}`, { responseType: 'text' });
+  }
+
+  getEmployeesList(): Observable<any> {
+    return this.http.get(`${this.baseUrl}`);
+  }
+}
---
+++ src/app/employee.ts
@@ -0,0 +1,31 @@
+export class Employee {
+  empId: number;
+  firstName: string;
+  lastName: string;
+   mailId:string;		 	
+  mobileNo:string;		 	
+ doB:string;
+  projectId:string;		 	 
+  projectName:string;		 	 
+  repManager:string;		 	 
+  projRole:string;		 	 
+exp:number;
+  aLine1:string;			
+  aLine2:string;			
+  city:string;			
+  state:string;	
+  country:string;
+   pinCode:string;
+  workLoc:string;			
+odcAccReq:string;			
+desReq:string;	
+ seatTag:string;
+l1Cert:string;	    
+ l2Cert:string;
+ primSkill:string;
+    secSkill:string;
+    active: boolean;
+
+}
+
+
